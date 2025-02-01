5ffd3995118db007b836ce443cb0181e21789c0c
SHIVIKA DHAMA <shivikadhama25@gmail.com>
2025-02-01 17:29:57 +0530

    Add files via upload

---
+++ p.js
@@ -0,0 +1,9 @@
+// Basic parallax effect on scroll
+window.addEventListener('scroll', () => {
+    const sections = document.querySelectorAll('.dimensional-section');
+    sections.forEach((section, index) => {
+      let speed = (index + 1) * 0.2;  // Gradually increase speed for each section
+      section.style.transform = `translateY(${window.scrollY * speed}px)`;
+    });
+  });
+  
