9ef8ad833da81ff8e5a6778810045d8fc5a61c65
SHIVIKA DHAMA <shivikadhama25@gmail.com>
2025-03-28 17:07:12 +0530

    Delete p.js

--- p.js
+++
@@ -1,9 +0,0 @@
-// Basic parallax effect on scroll
-window.addEventListener('scroll', () => {
-    const sections = document.querySelectorAll('.dimensional-section');
-    sections.forEach((section, index) => {
-      let speed = (index + 1) * 0.2;  // Gradually increase speed for each section
-      section.style.transform = `translateY(${window.scrollY * speed}px)`;
-    });
-  });
-  
